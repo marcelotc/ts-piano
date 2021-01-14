b7b29b0008106e7f3465b6b4d0a40e6d87f7ffcd
Marcelo Teixeira Cortes <marceloteixeiracortes@gmail.com>
2021-01-14 18:29:40 -0300

    done!

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { notes } from './helpers';
+import Ocatave from './Octave';
 
 function App() {
+  
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(e.currentTarget.value)
+    const audio = new Audio(`sounds/piano_${e.currentTarget.value}.mp3`);
+    audio.play();
+  }
+
   return (
     <div>
-     a
+      <Ocatave notes={notes} clickHandler={handleClick}></Ocatave>
     </div>
   );
 }
---
+++ src/Note.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { notes } from './helpers';
+
+const Black = styled.button`
+    width: 40px;
+    height: 130px;
+    background: black;
+    position: absolute;
+    margin: 1px;
+    margin-left: -20px;
+
+    :active {
+        background: #333;
+    }
+`;
+
+const White = styled.button`
+    width: 60px;
+    height: 200px;
+    background: white;
+    border: 1px solid black;
+    box-shadow: 2px 5px;
+    margin: 1px;
+
+    :active {
+        background: #eee;
+    }
+`;
+
+type Props = {
+    color: string;
+    note: string;
+    clickHandler: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Note: React.FC<Props> = ({ color, note, clickHandler }) => (
+    color === 'white' ? (
+        <White value={note} onClick={clickHandler}></White>
+    ) : (
+        <Black value={note} onClick={clickHandler}></Black>
+    )
+)
+
+export default Note;
